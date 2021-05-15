395ce73517accf249390678fd60676207c250b81
Anup <anuppoudel60@gmail.comm>
2021-05-15 11:15:20 +1000

    add cat added

---
+++ src/components/list-category/ListCategory.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { ListGroup } from "react-bootstrap";
+import { useSelector } from "react-redux";
+
+const ListCategory = () => {
+  return <div> </div>;
+};
+
+export default ListCategory;
--- src/pages/category/Category.js
+++ src/pages/category/Category.js
@@ -1,14 +1,24 @@
 import React from "react";
 import AddCategoryForm from "../../components/add-category-form/AddCategoryForm";
 import DefaultLayout from "../../components/layout/DefaultLayout";
+import { ListGroup } from "react-bootstrap";
+import "./Category.css";
 
 const Category = () => {
   return (
     <div className="dash" id="dashboard">
       <DefaultLayout>
         <AddCategoryForm />
-        <br />
-        <div className="display-cat"></div>
+        <hr />
+        <div className="display-cat">
+          <ListGroup className="listgroup">
+            <ListGroup.Item>Cras justo odio</ListGroup.Item>
+            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+            <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+            <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+          </ListGroup>
+        </div>
       </DefaultLayout>
     </div>
   );
