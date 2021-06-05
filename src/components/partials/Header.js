a97f0bc85381e27f84c416d09a783ae239f4d205
Anup <anuppoudel60@gmail.comm>
2021-06-06 00:04:22 +1000

    fonts updated

--- src/components/partials/Header.js
+++ src/components/partials/Header.js
@@ -28,59 +28,57 @@ const Header = () => {
 
   return (
     <>
-      <Navbar className="navbar navbar-expand-sm bg-dark navbar-dark">
-        <Navbar.Brand className="text-white nav-items ">Logo</Navbar.Brand>
-        <Nav className="ml">
-          <Link className="nav-items text-white" to="/">
-            Home
-          </Link>
+      <Navbar className="navbar navbar-expand-lg bg-dark navbar-dark">
+        <Navbar.Brand className="navbar-brand text-white nav-items ">
+          Logo
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-          <Link className="nav-items text-white">
-            <Dropdown>
-              <Dropdown.Toggle variant="success" id="dropdown-basic">
-                Shop by Category
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                {categoryList?.map((item, i) => {
-                  return (
-                    <div>
-                      <Dropdown.Item>
-                        <Link to={`/category/${item.slug}`}>{item.name}</Link>
-                      </Dropdown.Item>
-                    </div>
-                  );
-                })}
-              </Dropdown.Menu>
-            </Dropdown>
-          </Link>
-          <Link className="nav-items text-white" to="/checkout">
-            <FaCartPlus />
-          </Link>
+        <Link className="nav-link text-white" to="/">
+          Home
+        </Link>
 
-          <Form className="nav-items " inline>
-            {" "}
-            <Form.Control
-              className="mr-15"
-              type="text"
-              placeholder="Search products here..."
-            />
-          </Form>
+        <Link className="nav-link text-white">
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              Shop by Category
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              {categoryList?.map((item, i) => {
+                return (
+                  <div>
+                    <Dropdown.Item>
+                      <Link to={`/category/${item.slug}`}>{item.name}</Link>
+                    </Dropdown.Item>
+                  </div>
+                );
+              })}
+            </Dropdown.Menu>
+          </Dropdown>
+        </Link>
+        <Link className="nav-link text-white" to="/checkout">
+          <FaCartPlus />
+        </Link>
 
-          {isAuth ? (
-            <Link
-              className=" nav-items text-white"
-              onClick={handleOnLogout}
-              
-            >
-              <FiLogOut />
-              Logout
-            </Link>
-          ) : (
-            <Link className=" nav-items text-white" to="/login">
-              SignIn / SignUp
-            </Link>
-          )}
-        </Nav>
+        <Form className="form-inline">
+          {" "}
+          <Form.Control
+            className="mr-15"
+            type="text"
+            placeholder="Search products here..."
+          />
+        </Form>
+
+        {isAuth ? (
+          <Link className="nav-link text-white" onClick={handleOnLogout}>
+            <FiLogOut />
+            Logout
+          </Link>
+        ) : (
+          <Link className="nav-link text-white" to="/login">
+            SignIn / SignUp
+          </Link>
+        )}
       </Navbar>
     </>
   );
